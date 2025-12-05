14da920196b0148a98ee8b82cc6a70de8dd406a1
lucas07343 <lucassantosnunes07@gmail.com>
2025-12-04 21:58:13 -0300

    Adicionar programas de exemplo em Java

---
+++ programa4.java
@@ -0,0 +1,15 @@
+import java.util.Locale;
+
+public class programa4 {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        double a;
+        int b;
+        a = 5.0;
+        b = (int)a;
+
+        System.out.println(b);
+    }
+}
