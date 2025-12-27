b7684e1c18683df730aa1d306a0154d3d60d7c78
lucas07343 <lucassantosnunes07@gmail.com>
2025-12-27 18:32:44 -0300

    Corrigir erros de sintaxe e adicionar relatórios de totais e percentuais de cobaias

--- estrutura repetitiva/exercicio_experiencias.java
+++ estrutura repetitiva/exercicio_experiencias.java
@@ -18,23 +18,23 @@ public class exercicio_experiencias {
         Scanner sc = new Scanner(System.in);
 
         int n, qte_cobaias, total_coelhos, total_ratos, total_sapos, total_cobaias;
-        double percentual_coelhos percentual_ratos, percentual_sapos;
+        double percentual_coelhos, percentual_ratos, percentual_sapos;
         char tipo_cobaia;
 
         System.out.print("Quantos casos de teste serao digitados? ");
         n = sc.nextInt();
         System.out.println();
 
-        for (int i = 0, i < n, i++) {
+        total_coelhos = 0;
+        total_ratos = 0;
+        total_sapos = 0;
+        for (int i = 0; i < n; i++) {
             System.out.print("Quantidade de cobaias: ");
             qte_cobaias = sc.nextInt();
             System.out.print("Tipo de cobaia: ");
             tipo_cobaia = sc.next().charAt(0);
             tipo_cobaia = Character.toUpperCase(tipo_cobaia);
 
-            total_coelhos = 0;
-            total_ratos = 0;
-            total_sapos = 0;
             switch (tipo_cobaia) {
                 case 'C':
                     total_coelhos += qte_cobaias;
@@ -62,6 +62,19 @@ public class exercicio_experiencias {
         percentual_coelhos = (double) total_coelhos / total_cobaias * 100;
         percentual_ratos = (double) total_ratos / total_cobaias * 100;
         percentual_sapos = (double) total_sapos / total_cobaias * 100;
+
+        System.out.println();
+        System.out.println("RELATORIO FINAL:");
+        System.out.println();
+        System.out.println("Total: " + total_cobaias + " cobaias");
+        System.out.println();
+        System.out.println("Total de coelhos: " + total_coelhos);
+        System.out.println("Total de ratos: " + total_ratos);
+        System.out.println("Total de sapos: " + total_sapos);
+        System.out.println();
+        System.out.println("Percentual de coelhos: " + String.format("%.2f", percentual_coelhos));
+        System.out.println("Percentual de coelhos: " + String.format("%.2f", percentual_ratos));
+        System.out.println("Percentual de coelhos: " + String.format("%.2f", percentual_sapos));
 
         sc.close();
     }
