4a42693701ee8c21a9c0f23a7b38e5ad5d3280a8
lucas07343 <lucassantosnunes07@gmail.com>
2026-01-06 00:21:40 -0300

    Adicionar validação de gênero e temporizador no exercício de dados de pessoas

--- vetores/exercicio_dados_pessoas.java
+++ vetores/exercicio_dados_pessoas.java
@@ -2,13 +2,22 @@ import java.util.Scanner;
 import java.util.Locale;
 
 public class exercicio_dados_pessoas {
+
+    public static void temporizador(String[] args) {
+        try {
+            Thread.sleep(3000);
+        } 
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args) {
         
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        int n, qte_homens;
-        double soma_alturas, menor_altura, maior_altura, media_alturas_mulheres;
+        int n, qte_homens, qte_mulheres;
+        double soma_alturas_mulheres, menor_altura, maior_altura, media_alturas_mulheres;
 
         System.out.print("Quantas pessoas serao digitadas? ");
         n = sc.nextInt();
@@ -18,11 +27,20 @@ public class exercicio_dados_pessoas {
 
         System.out.println();
         for (int i = 0; i < n; i++) {
-            System.out.print("Altura da " + (i+1) + "a pessoa:");
+            System.out.print("Altura da " + (i+1) + "a pessoa: ");
             alturas[i] = sc.nextDouble();
-            System.out.print("Genero da " + (i+1) + "a pessoa:");
-            generos[i] = sc.next().charAt(0);
-            System.out.println();
+
+            do {
+                System.out.print("Genero da " + (i+1) + "a pessoa: ");
+                generos[i] = sc.next().charAt(0);
+                generos[i] = Character.toUpperCase(generos[i]);
+                System.out.println();
+
+                if (generos[i] != 'F' && generos[i] != 'M') {
+                    System.out.println("genero invalido, favor tente novamente");
+                    temporizador(args);
+                }
+            } while(generos[i] != 'F' && generos[i] != 'M');
         }
 
         menor_altura = alturas[0];
@@ -36,14 +54,22 @@ public class exercicio_dados_pessoas {
             }
         }
 
-        soma_alturas = 0;
+        soma_alturas_mulheres = 0;
+        qte_mulheres = 0;
         for (int i = 0; i < n; i++) {
-            soma_alturas += alturas[i];
-            
+            if (generos[i] == 'F') {
+                soma_alturas_mulheres += alturas[i];
+                qte_mulheres++;
+            }
         }
+
+        media_alturas_mulheres = soma_alturas_mulheres / qte_mulheres;
+        qte_homens = n - qte_mulheres;
 
         System.out.println("Menor altura = " + String.format("%.2f", menor_altura));
         System.out.println("Maior altura = " + String.format("%.2f", maior_altura));
+        System.out.println("Media das alturas das mulheres = " + String.format("%.2f", media_alturas_mulheres));
+        System.out.println("Numero de homens = " + qte_homens);
 
         sc.close();
     }
